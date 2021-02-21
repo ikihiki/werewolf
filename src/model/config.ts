a3e5d65bde1a658bf68d0b32e81d281ba8c861fd
伊吹英晃 <ibuki-90@hotmail.co.jp>
2021-02-21 23:56:56 +0900

    wip

--- src/model/config.ts
+++
@@ -1,8 +0,0 @@
-export interface Config {
-    numberOfWerewolf: number;
-    numberOfPsycho: number;
-    numberOfFortuneTeller:number;
-    numberOfKnight:number;
-    numberOfPsychic:number;
-    numberOfSharer:number;
-}
--- src/model/user.ts
+++
@@ -1,9 +0,0 @@
-export type UserId = string;
-
-export class User {
-  Id: UserId;
-
-  constructor (id: UserId) {
-    this.Id = id
-  }
-}
